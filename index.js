9fb48612e33a4b9c5fc5d2bb19981a3c72097d4b
fireworksflyaway <fireworksflyaway@live.com>
2017-04-21 17:03:30 +0800

    set up

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Mason Jackson in Office on 2017/4/21.
+ */
+import $ from 'jquery';
+
+let item=document.createElement("div");
+item.innerHTML='<h2>WebpackDemo</h2>';
+$('body').append('<h1>This is an ES6 app</h1>');
+document.body.appendChild(item);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Mason Jackson in Office on 2017/4/21.
+ */
+var HtmlWebpackPlugin=require('html-webpack-plugin');
+var modulePath=__dirname+"/modules";
+var entryPath=__dirname;
+var outPath=__dirname;
+module.exports = {
+    devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+    entry:  entryPath + "/index.js",
+    output: {
+        path: outPath,
+        filename: "bundle.js"
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.css$/,
+                loaders:['style-loader','css-loader']
+            },
+            {
+                test:/\.scss$/,
+                loaders:['style-loader','css-loader','sass-loader']
+            },
+            {
+                test:/\.(png|jpg|svg)$/,
+                loader:'url-loader?limit=40000'
+            },
+            {
+                test:/\.(eot|woff2|woff|ttf)$/,
+                loader:"file-loader"
+            },
+            {
+                test:/\.jsx?$/,
+                loader:'babel-loader',
+                query:{presets:['es2015']},
+                exclude: /node_modules/
+            }]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Hello World app'
+        })
+    ],
+    devServer:{
+        contentBase:"./build",   //入口地址目录
+        historyApiFallback:true,  //true代表跳转指向index.html
+        inline:true,    //源文件改变时自动刷新页面
+        port:8090  //服务器端口号
+    }
+}
