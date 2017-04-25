16dab8ea5eeebbe3a376fee3b4000b39937ba844
fireworksflyaway <fireworksflyaway@live.com>
2017-04-25 10:16:20 +0800

    add link on the logo img and add favicon.ico

--- modules/aside.js
+++ modules/aside.js
@@ -2,6 +2,7 @@
  * Created by Mason Jackson in Office on 2017/4/24.
  */
 import React from 'react';
+import {Link} from 'react-router';
 import '../style/aside.scss';
 
 import AsidePanel from './asidePanel';
@@ -11,10 +12,9 @@ export default class Aside extends React.Component{
     }
 
     render(){
-        console.log('aside render...');
         return(
            <aside className="col-lg-2 col-md-2 col-sm-1 col-xs-1 totalaside">
-               <img id="logoImg" src={require("../images/Logo.png")} />
+               <Link to="/"><img id="logoImg" src={require("../images/Logo.png")} /></Link>
                 <AsidePanel/>
            </aside>
         );
--- modules/content.js
+++ modules/content.js
@@ -15,7 +15,6 @@ export default class Content extends React.Component{
     }
 
     render(){
-        console.log('rendering...');
         return (
             <div>
                 <Aside />
