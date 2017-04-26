785e093b2bef32617369363f56ce51d032fd73d4
fireworksflyaway <fireworksflyaway@live.com>
2017-04-26 10:09:57 +0800

    把modal样式封装为CSS形式

--- modules/home.js
+++ modules/home.js
@@ -3,41 +3,6 @@
  */
 import React from 'react';
 import {Modal,Popover, Tooltip, OverlayTrigger} from 'react-overlays';
-//import '../node_modules/bootstrap/dist/js/bootstrap.min';
-let rand = ()=> (Math.floor(Math.random() * 20) - 10);
-
-const modalStyle = {
-    position: 'fixed',
-    zIndex: 1040,
-    top: 0, bottom: 0, left: 0, right: 0
-};
-
-const backdropStyle = {
-    position: 'fixed',
-    top: 0, bottom: 0, left: 0, right: 0,
-    zIndex: 'auto',
-    backgroundColor: '#000',
-    opacity: 0.5
-};
-
-const dialogStyle = function() {
-    // we use some psuedo random coords so nested modals
-    // don't sit right on top of each other.
-    let top = 50 + rand();
-    let left = 50 + rand();
-
-    return {
-        position: 'absolute',
-        width: 400,
-        top: top + '%', left: left + '%',
-        transform: `translate(-${top}%, -${left}%)`,
-        border: '1px solid #e5e5e5',
-        backgroundColor: 'white',
-        boxShadow: '0 5px 15px rgba(0,0,0,.5)',
-        padding: 20
-    };
-};
-
 
 const Home = React.createClass({
 
@@ -56,14 +21,11 @@ const Home = React.createClass({
 
                 <Modal
                     aria-labelledby='modal-label'
-                    style={modalStyle}
-                    backdropStyle={backdropStyle}
                     show={this.state.showModal}
                     onHide={this.close}
                 >
-                    <div style={dialogStyle()} >
-                        <h4 id='modal-label'>Text in a modal</h4>
-                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                    <div className="dialogStyle" >
+                        {this.props.children}
                     </div>
                 </Modal>
             </div>
