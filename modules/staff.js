c66ec979440cd5a271d0d262ffc2c63dc3f4658c
fireworksflyaway <fireworksflyaway@live.com>
2017-04-28 17:25:49 +0800

    添加staff表格

--- modules/staff.js
+++ modules/staff.js
@@ -41,10 +41,69 @@ export default class Staff extends React.Component{
                     </form>
                 </ArticleHeader>
                 <ArticleBody>
+                    <table className="table table-bordered table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>员工姓名</th>
+                                <th>工号</th>
+                                <th>部门</th>
+                                <th>职位</th>
+                                <th>级别</th>
+                                <th>电话</th>
+                                <th>邮箱</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>靳强</td>
+                                <td>1</td>
+                                <td>电商事业部</td>
+                                <td>主管</td>
+                                <td>P4</td>
+                                <td>18664867965</td>
+                                <td>fireworksflyaway@live.com</td>
+                            </tr>
+                            <tr>
+                                <td>靳强</td>
+                                <td>1</td>
+                                <td>电商事业部</td>
+                                <td>主管</td>
+                                <td>P4</td>
+                                <td>18664867965</td>
+                                <td>fireworksflyaway@live.com</td>
+                            </tr>
+                            <tr>
+                                <td>靳强</td>
+                                <td>1</td>
+                                <td>电商事业部</td>
+                                <td>主管</td>
+                                <td>P4</td>
+                                <td>18664867965</td>
+                                <td>fireworksflyaway@live.com</td>
+                            </tr>
+                            <tr>
+                                <td>靳强</td>
+                                <td>1</td>
+                                <td>电商事业部</td>
+                                <td>主管</td>
+                                <td>P4</td>
+                                <td>18664867965</td>
+                                <td>fireworksflyaway@live.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
+
+
+
                     <a href="#" onClick={this.handle.bind(this)}>Click to show modal</a>
                     <MyModal title="新建库房" ref="mm">
                         Hello <a href="./signin.html">go to sign in</a>
                     </MyModal>
+
+
+
                 </ArticleBody>
             </div>
         )
