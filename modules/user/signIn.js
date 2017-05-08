fa15e42ad10a70a880c83a7e8665da0dcaa8d113
fireworksflyaway <fireworksflyaway@live.com>
2017-05-08 11:28:41 +0800

    完成登录页设计

--- modules/user/signIn.js
+++ modules/user/signIn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import UserNav from './userNav';
-import '../../style/signIn.css';
+//import '../../style/signIn.css';
 
 export default class SignUp extends React.Component{
 	constructor(props){
@@ -77,72 +77,117 @@ export default class SignUp extends React.Component{
 		return(
 			<div>
 				<UserNav/>
-				<div id="section">
-					<div className="sec_box">
-						<div className="img fl">
-							<img src={require('../../images/signin.png')} />
-						</div>
-						<div className="signin_box fr">
-							<div className="title">
-								<span className="wrie"></span>
-								<span className='txt'>员工登录</span>
-								<span className='wrie'></span>
-							</div>
-							<form className="form-horizontal userForm" role="form">
-								<div className="form-group">
-									<label htmlFor="staffId" className="col-sm-2 control-label">工号</label>
-									<div className="col-sm-10">
-										<input type="text" className="form-control" id="staffIdInput" placeholder="请输入工号" />
-									</div>
-								</div>
-								<div className="form-group">
-									<label htmlFor="password" className="col-sm-2 control-label">密码</label>
-									<div className="col-sm-10">
-										<input type="password" className="form-control" id="passwordInput" placeholder="请输入密码" />
-									</div>
-								</div>
-								<div className="form-group">
-									<div className="col-sm-offset-2 col-sm-10">
-										<div className="checkbox">
-											<label>
-												<input type="checkbox" />记住当前账号
-											</label>
-										</div>
-									</div>
-								</div>
-								<div className="form-group">
-									<div className="col-sm-offset-2 col-sm-4">
-										<button type="submit" className="btn btn-primary userFormButton">登录</button>
-									</div>
-									<div className="col-sm-offset-2 col-sm-4">
-										<a href="./signup.html" className="btn btn-default userFormButton">注册</a>
-									</div>
-								</div>
-								<a href="#" className="col-sm-offset-2">忘记密码？</a>
-							</form>
-
-
-
-
-							{/*<div className="from ">*/}
-								{/*<div className='staffid line'>*/}
-									{/*<label>工&nbsp;&nbsp;号</label>*/}
-									{/*<input type='text' value={this.state.staffid} placeholder='请输入工号' onChange={this.onChangeInput.bind(this)}/>*/}
-								{/*</div>*/}
-								{/*<div className='line pwd'>*/}
-									{/*<label>密&nbsp;&nbsp;码</label>*/}
-									{/*<input type="password" value={this.state.pwd} placeholder='请输入密码' onChange={this.onChangeInput.bind(this)} />*/}
-									{/*<b onClick={this.onClickInputType.bind(this)}></b>*/}
-								{/*</div>*/}
-							{/*</div>*/}
-							{/*<div className='btns'>*/}
-								{/*<input  type='checkbox' checked={this.state.checked} onChange={this.onChangeCheckBox.bind(this)} /><span>记住当前账号</span>*/}
-								{/*<button className="signup btn" onClick={this.onBtnsClickSignUp.bind(this)}>登&nbsp;&nbsp;录</button>  <br />*/}
-								{/*<a href='#none'>忘记密码？</a>*/}
-							{/*</div>*/}
-						</div>
+				<section className="container">
+					<div className="col-lg-6 col-md-6 col-sm-6">
+						<img src={require('../../images/signin.png')} />
 					</div>
-				</div>
+					<div className="col-lg-6 col-md-6 col-sm-6">
+						<div className="title">
+							<span>员工登录</span>
+						</div>
+						<form className="form-horizontal userForm" role="form">
+							<div className="form-group">
+								<label htmlFor="staffId" className="col-sm-2 control-label">工号</label>
+								<div className="col-sm-10">
+									<input type="text" className="form-control" id="staffIdInput" placeholder="请输入工号" />
+								</div>
+							</div>
+							<div className="form-group">
+								<label htmlFor="password" className="col-sm-2 control-label">密码</label>
+								<div className="col-sm-10">
+									<input type="password" className="form-control" id="passwordInput" placeholder="请输入密码" />
+								</div>
+							</div>
+							<div className="form-group">
+								<div className="col-sm-offset-2 col-sm-10">
+									<div className="checkbox">
+										<label>
+											<input type="checkbox" />记住当前账号
+										</label>
+									</div>
+								</div>
+							</div>
+							<div className="form-group">
+								<div className="col-sm-offset-2 col-sm-4">
+									<button type="submit" className="btn btn-primary userFormButton">登录</button>
+								</div>
+								<div className="col-sm-offset-2 col-sm-4">
+									<a href="./signup.html" className="btn btn-default userFormButton">注册</a>
+								</div>
+							</div>
+							<a href="#" className="col-sm-offset-2">忘记密码？</a>
+						</form>
+					</div>
+				</section>
+
+
+
+				{/*<div id="section">*/}
+					{/*<div className="sec_box">*/}
+						{/*<div className="img fl">*/}
+							{/*<img src={require('../../images/signin.png')} />*/}
+						{/*</div>*/}
+						{/*<div className="signin_box fr">*/}
+							{/*<div className="title">*/}
+								{/*<span className="wrie"></span>*/}
+								{/*<span className='txt'>员工登录</span>*/}
+								{/*<span className='wrie'></span>*/}
+							{/*</div>*/}
+							{/*<form className="form-horizontal userForm" role="form">*/}
+								{/*<div className="form-group">*/}
+									{/*<label htmlFor="staffId" className="col-sm-2 control-label">工号</label>*/}
+									{/*<div className="col-sm-10">*/}
+										{/*<input type="text" className="form-control" id="staffIdInput" placeholder="请输入工号" />*/}
+									{/*</div>*/}
+								{/*</div>*/}
+								{/*<div className="form-group">*/}
+									{/*<label htmlFor="password" className="col-sm-2 control-label">密码</label>*/}
+									{/*<div className="col-sm-10">*/}
+										{/*<input type="password" className="form-control" id="passwordInput" placeholder="请输入密码" />*/}
+									{/*</div>*/}
+								{/*</div>*/}
+								{/*<div className="form-group">*/}
+									{/*<div className="col-sm-offset-2 col-sm-10">*/}
+										{/*<div className="checkbox">*/}
+											{/*<label>*/}
+												{/*<input type="checkbox" />记住当前账号*/}
+											{/*</label>*/}
+										{/*</div>*/}
+									{/*</div>*/}
+								{/*</div>*/}
+								{/*<div className="form-group">*/}
+									{/*<div className="col-sm-offset-2 col-sm-4">*/}
+										{/*<button type="submit" className="btn btn-primary userFormButton">登录</button>*/}
+									{/*</div>*/}
+									{/*<div className="col-sm-offset-2 col-sm-4">*/}
+										{/*<a href="./signup.html" className="btn btn-default userFormButton">注册</a>*/}
+									{/*</div>*/}
+								{/*</div>*/}
+								{/*<a href="#" className="col-sm-offset-2">忘记密码？</a>*/}
+							{/*</form>*/}
+
+
+
+
+							{/*/!*<div className="from ">*!/*/}
+								{/*/!*<div className='staffid line'>*!/*/}
+									{/*/!*<label>工&nbsp;&nbsp;号</label>*!/*/}
+									{/*/!*<input type='text' value={this.state.staffid} placeholder='请输入工号' onChange={this.onChangeInput.bind(this)}/>*!/*/}
+								{/*/!*</div>*!/*/}
+								{/*/!*<div className='line pwd'>*!/*/}
+									{/*/!*<label>密&nbsp;&nbsp;码</label>*!/*/}
+									{/*/!*<input type="password" value={this.state.pwd} placeholder='请输入密码' onChange={this.onChangeInput.bind(this)} />*!/*/}
+									{/*/!*<b onClick={this.onClickInputType.bind(this)}></b>*!/*/}
+								{/*/!*</div>*!/*/}
+							{/*/!*</div>*!/*/}
+							{/*/!*<div className='btns'>*!/*/}
+								{/*/!*<input  type='checkbox' checked={this.state.checked} onChange={this.onChangeCheckBox.bind(this)} /><span>记住当前账号</span>*!/*/}
+								{/*/!*<button className="signup btn" onClick={this.onBtnsClickSignUp.bind(this)}>登&nbsp;&nbsp;录</button>  <br />*!/*/}
+								{/*/!*<a href='#none'>忘记密码？</a>*!/*/}
+							{/*/!*</div>*!/*/}
+						{/*</div>*/}
+					{/*</div>*/}
+				{/*</div>*/}
 			</div>
 		)
 	}
